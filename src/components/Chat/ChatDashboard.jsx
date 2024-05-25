39ae64686d68dd0ba16b2403f9782eec46395a3d
webdev-faizan <webdev.faizanali@gmail.com>
2024-05-25 09:23:05 -0400

    remove unsed code

--- src/components/Chat/ChatDashboard.jsx
+++ src/components/Chat/ChatDashboard.jsx
@@ -19,7 +19,7 @@ const ChatDashboard = () => {
       <Box
         sx={{
           height: "100vh",
-          overflowY: "scroll",
+          overflowY: "auto",
           width: "360px",
           background: "#F8FAFF",
           "box-shadow": " 0px 0px 4px 0px rgba(0, 0, 0, 0.25)",
