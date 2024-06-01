1897287f8eca949aca611eb2635bc833823e2114
webdev-faizan <webdev.faizanali@gmail.com>
2024-06-01 15:12:18 -0400

    change loca

--- src/routes/IndexRoutes.jsx
+++ src/routes/IndexRoutes.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
 import React, { createContext, useRef } from "react";
-import Chat from "../pages/Chat";
 import SideNav from "../components/SideNav";
 import VideoCall from "../components/VideoCalling/p2p/VideoCall";
 import SnackbarCallInfo from "../components/SnackbarCallInfo";
 import AudioCall from "../components/AudioCall/p2p/AudioCall";
+import Chat from "../pages/Chat";
 import NotFound from "../pages/Error/NotFound";
 import Index from "../pages/Index";
 export const P2PCallContext = createContext();
