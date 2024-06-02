e0bcb5f8d9e427de3476ccce4828913ea8ec44f3
webdev-faizan <webdev.faizanali@gmail.com>
2024-06-02 15:26:52 -0400

    used env

--- src/service/user.js
+++ src/service/user.js
@@ -11,8 +11,8 @@ export const uploadUserImage = async (e) => {
   if (!image.type.startsWith("image/")) return;
   let formData = new FormData();
   formData.append("file", image);
-  formData.append("cloud_name", "dkhgfsefj");
-  formData.append("upload_preset", "chating");
+  formData.append("cloud_name", process.env.REACT_APP_CLOUD_NAME);
+  formData.append("upload_preset", process.env.REACT_APP_UPLOAD_PRESENT);
   const response = await fetch(
     "https://api.cloudinary.com/v1_1/dkhgfsefj/upload",
     {
